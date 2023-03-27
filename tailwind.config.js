6e5f22a2c7b48bfd7564e9dc7f8b62fea31cedf9
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-03-27 18:28:13 +0200

    feat: add 4daagse page and minor tweaks

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html"],
+  content: ["./index.html", "./avond-4daagse-2023/index.html"],
   theme: {
     extend: {},
   },
