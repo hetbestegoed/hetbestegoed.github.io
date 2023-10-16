8ca9306e5487ca3ff70606fd3a4e8eca3c8bda29
Brian van Burken <brianvanburken@users.noreply.github.com>
2023-10-16 20:39:09 +0200

    fix: build css with right dir

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,6 +1,6 @@
 /** @type {import('tailwindcss').Config} */
 module.exports = {
-  content: ["./index.html", "./avond-4daagse-2023/index.html"],
+  content: ["./index.html", "./avond-4daagse-elst/index.html"],
   theme: {
     extend: {},
   },
